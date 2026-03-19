90282115fd986574a714660663fa72e584bfd774
Ravish Gupta <206032129+RavishGupta194@users.noreply.github.com>
2026-03-19 15:30:59 +0530

    Time: 2111 ms (7.49%) | Memory: 7.8 MB (54.48%) - LeetSync

---
+++ 507-perfect-number/perfect-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int count=0;
+        for(int i=1;i<num-1;i++){
+            if(num%i==0){
+                count+=i;
+            }
+        }
+        return num==count;
+    }
+};
