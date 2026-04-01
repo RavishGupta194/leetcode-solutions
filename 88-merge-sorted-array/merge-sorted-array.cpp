16140c47e80c068d772ced73e2fd8028e983a7ce
Ravish Gupta <206032129+RavishGupta194@users.noreply.github.com>
2026-04-01 19:21:27 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (41.03%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,10 +1,15 @@
-
-    class Solution {
+class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int i = 0; i < n; i++) {
-            nums1[m + i] = nums2[i];
+        while(!nums1.empty() && nums1.back()==0 && nums1.size()>m){
+            nums1.pop_back();
         }
-        sort(nums1.begin(), nums1.end());
+        while(!nums2.empty()&&nums2.back()==0 && nums2.size()>n){
+            nums2.pop_back();
+        }
+        for(int i=0;i<nums2.size();i++){
+            nums1.push_back(nums2[i]);
+        }
+        sort(nums1.begin(),nums1.end());
     }
 };
