477d95de7f3c19066ca6ed7deb5f24a6a57ae45b
Ravish Gupta <206032129+RavishGupta194@users.noreply.github.com>
2026-05-17 23:32:39 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (20.68%) - LeetSync

---
+++ 3582-find-indices-of-stable-mountains/find-indices-of-stable-mountains.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> stableMountains(vector<int>& height, int threshold) {
+        vector<int>temp;
+        
+        for(int i = 0; i<height.size();i++){
+            if(i>0 && (height[i-1]>threshold)){
+                temp.push_back(i);
+            }
+        }
+        return temp;
+    }
+};
